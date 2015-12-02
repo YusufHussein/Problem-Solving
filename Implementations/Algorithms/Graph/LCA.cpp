d9f0e4dba5995ae4cf2a8e213787d83a17e9f585
YusufHussein <yusuf.hussein8894@gmail.com>
2015-12-02 16:27:46 +0200

    Update LCA.cpp

--- Implementations/Algorithms/Graph/LCA.cpp
+++ Implementations/Algorithms/Graph/LCA.cpp
@@ -1,27 +1,41 @@
-class FenwickTree
-{
-#define data_type int
-#define LSOne(a) ((a)&(-(a)))
-private:
-    vector<data_type> ft;
-public:
-    FenwickTree(data_type n)
-    {
-        ft.assign(n + 1, 0);
+/// LCA
+int n,l;
+vector <vector <int> > G;
+vector <int> tin, tout;
+int timer;
+vector <vector <int> > up;
+
+void DFS (int v, int P = 0) {
+    tin [v] = ++ timer;
+    up [v] [0] = P;
+    for (int i = 1; i <= l; i ++)
+        up [v] [i] = up [up [v] [i-1]] [i-1];
+    for (size_t i = 0 ; i <G [v] .size (); i ++) {
+        int to  = G [v] [i];
+        if (to!= P)
+            DFS (to, v);
     }
-    data_type rsq(int b){
-        data_type sum = 0;
-        for (; b; b -= LSOne(b)) sum += ft[b];
-        return sum;
-    }
-    data_type rsq(int a, int b){
-        return rsq(b) - (a == 1 ? 0 : rsq(a - 1));
-    }
-    void adjust(int k, data_type v){
-        for (; k < (int)ft.size(); k += LSOne(k)) ft[k] += v;
-    }
-    void set(int k,data_type v){
-        data_type diff= v - rsq(k,k);
-        adjust(k,diff);
-    }
-};
+    tout [v] = ++ timer;
+}
+
+bool upper (int a, int b) {
+    return tin [a] <= tin [b] && tout [a]>= tout [b];
+}
+
+int lca (int a, int b) {
+    if (upper (a, b)) return a;
+    if (upper (b, a)) return b;
+    for (int i = l; i>= 0; --i)
+        if (! upper (up [a] [i], b))
+            a = up [a] [i];
+    return up [a][0];
+}
+/// LCA
+
+///LCA main
+    tin.resize (n), tout.resize (n), up.resize (n);
+    l = 1;
+    while ((1 << l) <= n) ++ l;
+    for (int i = 0; i <n; i ++) up [i] .resize (l + 1);
+    DFS (0);
+///LCA
